58b4cc56e94d3ca565bc597b2ab0e56493434473
Victor Wang <wjp2013@gmail.com>
2025-10-24 21:57:18 +0800

    feat: LinkFilter 支持过滤用户名
    - 添加 @username 格式的用户名过滤功能
    - 支持过滤 @字母、数字、下划线组合的用户名
    - 自动清理移除用户名后产生的多余空白

--- filters/link_filter.py
+++ filters/link_filter.py
@@ -7,16 +7,17 @@ logger = logging.getLogger(__name__)
 
 class LinkFilter(BaseFilter):
     """
-    删除消息中的超链接，同时保留可读文本
+    删除消息中的超链接和用户名，同时保留可读文本
     """
 
     _MARKDOWN_LINK_PATTERN = re.compile(r'\[([^\]]+)\]\((https?|tg):\/\/[^\s)]+?\)')
     _HTML_LINK_PATTERN = re.compile(r'<a\s+href="[^"]*">(.*?)<\/a>', re.IGNORECASE | re.DOTALL)
     _PLAIN_LINK_PATTERN = re.compile(r'(https?|tg):\/\/\S+')
+    _USERNAME_PATTERN = re.compile(r'@[a-zA-Z0-9_]+')
 
     async def _process(self, context):
         """
-        移除 Markdown、HTML 与纯文本形式的超链接
+        移除 Markdown、HTML 与纯文本形式的超链接，以及用户名
         """
         message_text = context.message_text
         if not message_text:
@@ -33,13 +34,16 @@ class LinkFilter(BaseFilter):
         # 纯文本 URL http(s)/tg -> 删除
         message_text = self._PLAIN_LINK_PATTERN.sub('', message_text)
 
+        # 用户名 @username -> 删除
+        message_text = self._USERNAME_PATTERN.sub('', message_text)
+
         # 清理多余空白
         message_text = re.sub(r'[ \t]+', ' ', message_text)
         message_text = re.sub(r'\s+\n', '\n', message_text)
         message_text = re.sub(r'\n{3,}', '\n\n', message_text).strip()
 
         if message_text != original_text:
-            logger.info('LinkFilter 移除了消息中的超链接')
+            logger.info('LinkFilter 移除了消息中的超链接和用户名')
 
         context.message_text = message_text
         context.check_message_text = message_text
